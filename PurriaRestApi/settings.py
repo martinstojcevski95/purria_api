6a48406d2a8f10287195f15fa960415f2492484d
martin <stojcevski.m95@gmail.com>
2022-11-16 11:33:35 +0100

    removed default sql

--- PurriaRestApi/settings.py
+++ PurriaRestApi/settings.py
@@ -113,10 +113,6 @@ DATABASES = {
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
-   # 'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
 }
 
 ALLOWED_HOSTS = ['*']
